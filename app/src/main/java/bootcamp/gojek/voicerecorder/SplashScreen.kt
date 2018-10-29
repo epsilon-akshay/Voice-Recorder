f01ce21047984571810c0331097aa4c6a1d22792
akshay.nambiar <akshay.nambiar@go-jek.com>
2018-10-29 13:02:55 +0530

    splash screen will take you to the next activty

--- app/src/main/java/bootcamp/gojek/voicerecorder/SplashScreen.kt
+++ app/src/main/java/bootcamp/gojek/voicerecorder/SplashScreen.kt
@@ -1,12 +1,18 @@
 package bootcamp.gojek.voicerecorder
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            val nextActivity = Intent(this, RecordingActivity::class.java)
+            startActivity(nextActivity)
+        },1000)
     }
 }
