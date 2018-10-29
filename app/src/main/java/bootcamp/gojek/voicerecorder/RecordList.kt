c33c845ba412bf9ba49609cb5b015eade7a7f59b
akshay.nambiar <akshay.nambiar@go-jek.com>
2018-10-29 13:00:44 +0530

    added splash screen

--- app/src/main/java/bootcamp/gojek/voicerecorder/RecordList.kt
+++ app/src/main/java/bootcamp/gojek/voicerecorder/RecordList.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.os.Environment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import java.io.File
 
 class RecordList : AppCompatActivity() {
